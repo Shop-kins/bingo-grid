896248488840fa9c10522ad2404a6900de2e87fc
Samuel Hopkins <samuel.hopkins@anaplan.com>
2018-06-27 17:05:00 +0100

    Refining

--- src/main/kotlin/bingogrid/GridGenerator.kt
+++ src/main/kotlin/bingogrid/GridGenerator.kt
@@ -2,60 +2,57 @@ package bingogrid
 
 import java.util.*
 
-class GridGenerator(val seed: Double){
+class GridGenerator(val seed: Double) {
 
     private val gridSize: Int = 5
+    private val difficulty: Int = 4
 
-    fun generate(): List<List<Int>>{
-        val first = numToList((seed*10).toInt())
-        val second = numToList(((1-seed)*10).toInt())
-        val third = generateComplexRow(first, second)
-        val fourth = generateComplexRow(first, second, third)
-        val fifth = generateComplexRow(first, second, third, fourth)
-        return listOf(first, second, third, fourth, fifth)
+    fun generate(): List<List<Int>> {
+        val gridRows = mutableListOf<List<Int>>()
+        (1..gridSize).forEach {
+            gridRows += generateGridRow(gridRows)
+        }
+        return gridRows
     }
 
-
-
-    fun numToList(num: Int): List<Int> =
-        when (num) {
-            0 -> 3
-            1 -> 5
-            2 -> 6
-            3 -> 9
-            4 -> 10
-            5 -> 12
-            6 -> 17
-            7 -> 18
-            8 -> 20
-            9 -> 24
-            else -> 3
-        }.toString(2).padEnd(5, '0').map(Character::getNumericValue)
-
-    private fun generateComplexRow(vararg numList: List<Int>): List<Int>{
+    private fun generateGridRow(gridRows: List<List<Int>>): List<Int> {
         val rand = Random(seed.toLong())
-        val safe = safeColumns(*numList)
-        val randOne = safe[rand.nextInt(safe.size)]
-        safe.removeAll{ it == randOne}
-        val randTwo = safe[rand.nextInt(safe.size)]
+        val safe = safeColumns(gridRows)
         val nextRow = generateZeroList()
-        nextRow[randOne] = 1
-        nextRow[randTwo] = 1
+        for (i in 1..difficulty) {
+            val necessary = necessaryColumn(gridRows, safe)
+            if(necessary != null) {
+                nextRow[necessary] = 1
+                safe.removeAll { it == necessary }
+            } else {
+                val columnNum = safe[rand.nextInt(safe.size)]
+                nextRow[columnNum] = 1
+                safe.removeAll { it == columnNum }
+            }
+        }
         return nextRow
     }
 
-    private fun generateZeroList(): MutableList<Int> = (1..gridSize).map{0}.toMutableList()
+    private fun generateZeroList(): MutableList<Int> = (1..gridSize).map { 0 }.toMutableList()
 
-    private fun safeColumns(vararg numList: List<Int>): MutableList<Int>{
+    private fun safeColumns(gridRows: List<List<Int>>): MutableList<Int> {
         val safeList = mutableListOf<Int>()
-        for(i in 1..gridSize-1){
-            if(numList.sumBy { it[i] } < 2) safeList.add(i)
-            if(numList.sumBy { it[i] } < 1) safeList.add(i)
+        for (i in 0 until gridSize) {
+            for (j in 0 until (difficulty - gridRows.sumBy { it[i] })) {
+                safeList.add(i)
+            }
         }
         return safeList
     }
 
-
+    private fun necessaryColumn(gridRows: List<List<Int>>, safe: List<Int>): Int? {
+        for (i in 0 until gridSize) {
+            if(safe.contains(i)){
+                if (difficulty - gridRows.sumBy { it[i] } == gridSize) return i
+            }
+        }
+        return null
+    }
 
 
 }
